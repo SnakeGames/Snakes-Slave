3e11330e9b6a7880f8fb41864978e48bd33cf0bc
SnakeGames <61299588+SnakeGames@users.noreply.github.com>
2020-02-20 17:41:51 -0800

    test

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
-const Discord = require("discord.js")
-const Client  = new Discord.Client();
-const prefix = "-"
-
-Client.on('Ready', ()=>{
-    console.log("The bot is online");
-})
-
-Client.on('message', (message)=>{
-    if(-message.content.startsWith.apply(prefix)) return;
-    if(message.author.bot) return;
-
-
-    if(message.content.startsWith(prefix + "ip"))
-        message.reply("Currently there is no IP for the server. We are working on the server!");
-
-    if(message.content.startsWith(prefix + "help"))
-        message.reply("The commands are (CHANGE THIS AFTER YOU MAKE THE BOT)");
-
-    if(message.content.startsWith(prefix + "version"))
-        message.reply("The server version is, 1.8.8 (1.8.9 also works)");
-
-    if(message.content.startsWith(prefix + "apply"))
-        message.author.send("Hello! Thank you for considering to apply, but unfortunatly the staff applications are closed. There will be an announcement when the staff applications are open! Good luck!");
-
-
-
-
-
-})
-
-
-Client.login("Njc5ODE3NTEyNTMxMDAxMzU5.Xk23bg.AkBlMlruanYlsczayC-eJLU7iJQ")
+{
+  "name": "Snakes-Slave",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/SnakeGames/Snakes-Slave.git"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/SnakeGames/Snakes-Slave/issues"
+  },
+  "homepage": "https://github.com/SnakeGames/Snakes-Slave#readme"
+}
